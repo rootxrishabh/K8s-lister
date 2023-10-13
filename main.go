de6d7c050bd604aa12f39453b165c30e5ba56ff0
rootxrishabh <risrock02@gmail.com>
2023-10-13 16:46:39 +0530

    Pod creator added

--- main.go
+++ main.go
@@ -5,11 +5,58 @@ import (
 	"flag"
 	"fmt"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+
+// Function to call in case you want to create a pod. NOTE: this function is optional and it does not add to the purpose of this project. It was created just for learning purposes.
+func k8sClient(){
+	config := flag.String("Kubeconfig", "/Users/rootxrishabh/.kube/config", "Kubeconfig from hostOS")
+	kubeconfig, err := clientcmd.BuildConfigFromFlags("", *config)
+	if err != nil{
+		fmt.Printf("Error %s deriving config from host", err.Error())
+		kubeconfig, err = rest.InClusterConfig()
+		if err != nil{
+			fmt.Printf("Error %s deriving config from cluster. Check roles!", err.Error())
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil{
+		fmt.Printf("Error creating client set from config")
+	}
+
+	pod := v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "mysql-pod",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "mysql",
+					Image: "mysql",
+					Env: []v1.EnvVar{
+						{
+							Name:  "MYSQL_ROOT_PASSWORD",
+							Value: "your-root-password",
+						},
+					},
+					Ports: []v1.ContainerPort{
+						{
+							ContainerPort: 3306,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	clientset.CoreV1().Pods("default").Create(context.Background(), &pod, metav1.CreateOptions{})
+}
 
 func main() {
 	kubeconf := flag.String("kubeconfing", "/Users/rootxrishabh/.kube/config", "location to kubernetes config file")
@@ -30,7 +77,7 @@ func main() {
 		fmt.Printf("Encountered an error while creating a clientset\n", err.Error())
 	}
 
-	pod, err := clientset.CoreV1().Pods("default").List(context.Background(), v1.ListOptions{})
+	pod, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		fmt.Printf("Encountered retrieveing pods\n", err.Error())
 	}
@@ -40,20 +87,20 @@ func main() {
 	}
 
 	// Getting deployments via the clientset
-	deployment, err := clientset.AppsV1().Deployments("default").List(context.Background(), v1.ListOptions{})
+	deployment, err := clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		fmt.Printf("Encountered retrieveing deployments\n", err.Error())
 	}
 
 	// prints the label on the node
-	resource, err := clientset.CoreV1().Nodes().Get(context.Background(), "kind-control-plane", v1.GetOptions{})
+	resource, err := clientset.CoreV1().Nodes().Get(context.Background(), "kind-control-plane", metav1.GetOptions{})
 	if err != nil {
 		fmt.Printf("Error %s encountered while getting node", err.Error())
 	}
 	i := 1
 	for nodes := range resource.GetLabels() {
 		fmt.Printf("Labels no %v on the node is %v \n", i, nodes)
-		i++
+		i++``
 	}
 
 	for _, dep := range deployment.Items {
@@ -61,26 +108,4 @@ func main() {
 	}
 	fmt.Print("\n")
 
-	// Creating an informer factory which will help keep a realtime track of changes inside the cluster for various resources
-	// informerFactory := informers.NewSharedInformerFactory(clientset, 10*time.Minute)
-	// podInformer := informerFactory.Core().V1().Pods()
-	// podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-	// 	AddFunc: func(new interface{}) {
-	// 		fmt.Printf("Write your bussiness logic to handle AddFunc\n")
-	// 	},
-	// 	UpdateFunc: func(oldObj, newObj interface{}) {
-	// 		fmt.Printf("Write your bussiness logic to handle UpdateFunc\n")
-	// 	},
-	// 	DeleteFunc: func(obj interface{}) {
-	// 		fmt.Printf("Write your bussiness logic to handle DeleteFunc\n")
-	// 	},
-	// })
-
-	// informerFactory.Start(wait.NeverStop)
-	// informerFactory.WaitForCacheSync(wait.NeverStop)
-	// if realTimePodInfo, err := podInformer.Lister().Pods("default").Get("nginx"); err != nil {
-	// 	fmt.Printf("Error %s while retrieveing pods from he informer", err.Error())
-	// } else {
-	// 	fmt.Printf("Pods retrieved %T", realTimePodInfo)
-	// }
 }
